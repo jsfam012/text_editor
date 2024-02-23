639f5f4fcdf4da627b0cd9f803ff7a3ae5136efb
Sean Ahn <edotakeout@gmail.com>
2024-02-23 17:41:31 -0500

    updated sw, config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,6 +6,8 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
+
+
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +20,36 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: './src-sw.js',
+      }), 
+      new WebpackPwaManifest({
+        // TODO: Create a manifest.json:
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
